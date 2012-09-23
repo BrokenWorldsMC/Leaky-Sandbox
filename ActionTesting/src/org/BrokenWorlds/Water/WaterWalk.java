31f3a6ae6c3c784224a65803e814fd0eb17c3c0a
xIGBClutchIx <lockerzmodding@gmail.com>
2012-09-23 19:57:14 -0300

    Completely Rewrote
    Uses Block position instead some complex math crap. 
    Also its now a 3x3 square of lilies, this helps the player not to fall off.

--- ActionTesting/src/org/BrokenWorlds/Water/WaterWalk.java
+++ ActionTesting/src/org/BrokenWorlds/Water/WaterWalk.java
@@ -1,14 +1,10 @@
 package org.BrokenWorlds.Water;
 
-import java.util.List;
-import java.util.ListIterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
@@ -18,259 +14,159 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.Vector;
 
-import com.google.common.collect.Lists;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import org.bukkit.block.BlockState;
+import org.bukkit.event.EventPriority;
 
 public class WaterWalk extends JavaPlugin implements Listener {
 
-	private static final Logger logger = Logger.getLogger("Minecraft");
-	private List<String> waterWalking = Lists.newArrayList();
-	private List<Location> blocks = Lists.newArrayList();
+    private static final Logger logger = Logger.getLogger("Minecraft");
+    private HashMap<String, Lilies> lilywalkers;
 
-	@Override
-	public void onEnable() {
-		getServer().getPluginManager().registerEvents(this, this);
-		logger.log(Level.SEVERE, "Water Walk Enabled!");
-	}
+    @Override
+    public void onEnable() {
+        lilywalkers = new HashMap<String, Lilies>();
+        getServer().getPluginManager().registerEvents(this, this);
+        logger.log(Level.SEVERE, "Water Walk Enabled!");
+    }
 
-	@Override
-	public void onDisable() {
-		logger.log(Level.SEVERE, "Water Walk Disabled!");
-	}
+    @Override
+    public void onDisable() {
+        logger.log(Level.SEVERE, "Water Walk Disabled!");
+    }
 
-	@EventHandler
-	public void onPlayerMove(PlayerMoveEvent event) {
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Lilies lilies = lilywalkers.get(event.getPlayer().getName());
+        if (lilies != null) {
+            Block block = event.getTo().getBlock();
+            boolean moved = lilies.isMoved(block);
+            if (moved) {
+                lilies.move(block);
+            }
+        }
+    }
 
-		if (waterWalking.contains(event.getPlayer().toString())) {
-			// water walking! Update lily pads below feet on water
-			Player player = event.getPlayer();
-			Location loc = player.getLocation();
-			Location cloneLoc = loc.clone();
-			World world = player.getWorld();
-			Vector direction = loc.getDirection();
-			Block block = null;
-			//logger.info("Direction X: " + direction.getX() + " Direction Z: " + direction.getZ());
-			// Player is moving quickly, add 3 or 4 lily pads
-			if (player.isSprinting() && world.getBlockAt( (int)cloneLoc.getX(), (int)cloneLoc.getY() - 1, (int)cloneLoc.getZ()).isLiquid()) {
-				// Player is moving North, (-1, 0)
-				if (Math.round(direction.getX()) == 0 && Math.round(direction.getZ()) == 1){
-					// add the blocks in the direction player is headed
-					blocks.add(new Location(world, cloneLoc.getX(), cloneLoc.getY(), cloneLoc.getZ() + 1.0D));
-					blocks.add(new Location(world, cloneLoc.getX(), cloneLoc.getY(), cloneLoc.getZ() + 2.0D));
-					blocks.add(new Location(world, cloneLoc.getX(), cloneLoc.getY(), cloneLoc.getZ() + 3.0D));
-					
-					// Go through list of locations and set the block to lily pad
-					for(Location tempLoc : blocks){
-						block = world.getBlockAt(tempLoc);
-						if(block.isEmpty() && block.getRelative(BlockFace.DOWN).isLiquid()){
-							block.setTypeId(111);
-							block.setMetadata("waterWalkLily", new FixedMetadataValue(this, true));
-						}
-					}
-					
-				}
-				// Player is moving East, (-1,0)
-				if (Math.round(direction.getX()) == -1 && Math.round(direction.getZ()) == 0){
-					// add the blocks in the direction player is headed
-					blocks.add(new Location(world, cloneLoc.getX() - 1.0D, cloneLoc.getY(), cloneLoc.getZ()));
-					blocks.add(new Location(world, cloneLoc.getX() - 2.0D, cloneLoc.getY(), cloneLoc.getZ()));
-					blocks.add(new Location(world, cloneLoc.getX() - 3.0D, cloneLoc.getY(), cloneLoc.getZ()));
-					
-					// Go through list of locations and set the block to lily pad
-					for(Location tempLoc : blocks){
-						block = world.getBlockAt(tempLoc);
-						if(block.isEmpty() && block.getRelative(BlockFace.DOWN).isLiquid()){
-							block.setTypeId(111);
-							block.setMetadata("waterWalkLily", new FixedMetadataValue(this, true));
-						}
-					}
-					
-				}
-				// Player is moving South, (0, -1)
-				if (Math.round(direction.getX()) == 0 && Math.round(direction.getZ()) == -1){
-					// add the blocks in the direction player is headed
-					blocks.add(new Location(world, cloneLoc.getX(), cloneLoc.getY(), cloneLoc.getZ() - 1.0D));
-					blocks.add(new Location(world, cloneLoc.getX(), cloneLoc.getY(), cloneLoc.getZ() - 2.0D));
-					blocks.add(new Location(world, cloneLoc.getX(), cloneLoc.getY(), cloneLoc.getZ() - 3.0D));
-					
-					// Go through list of locations and set the block to lily pad
-					for(Location tempLoc : blocks){
-						block = world.getBlockAt(tempLoc);
-						if(block.isEmpty() && block.getRelative(BlockFace.DOWN).isLiquid()){
-							block.setTypeId(111);
-							block.setMetadata("waterWalkLily", new FixedMetadataValue(this, true));
-						}
-					}
-				}
-					
-				// Player is moving West, (1, 0)
-				if (Math.round(direction.getX()) == 1 && Math.round(direction.getZ()) == 0){
-					// add the blocks in the direction player is headed
-					blocks.add(new Location(world, cloneLoc.getX() + 1.0D, cloneLoc.getY(), cloneLoc.getZ()));
-					blocks.add(new Location(world, cloneLoc.getX() + 2.0D, cloneLoc.getY(), cloneLoc.getZ()));
-					blocks.add(new Location(world, cloneLoc.getX() + 3.0D, cloneLoc.getY(), cloneLoc.getZ()));
-					
-					// Go through list of locations and set the block to lily pad
-					for(Location tempLoc : blocks){
-						block = world.getBlockAt(tempLoc);
-						if(block.isEmpty() && block.getRelative(BlockFace.DOWN).isLiquid()){
-							block.setTypeId(111);
-							block.setMetadata("waterWalkLily", new FixedMetadataValue(this, true));
-						}
-					}
-				}
-			}
-			// Player is walking slowly, only add two lily pads
-			else {
-				// Player is moving North, (0, 1)
-				if (Math.round(direction.getX()) == 0 && Math.round(direction.getZ()) == 1){
-					// add the blocks in the direction player is headed
-					blocks.add(new Location(world, cloneLoc.getX(), cloneLoc.getY(), cloneLoc.getZ() + 1.0D));
-					blocks.add(new Location(world, cloneLoc.getX(), cloneLoc.getY(), cloneLoc.getZ() + 2.0D));
-					
-					// Go through list of locations and set the block to lily pad
-					for(Location tempLoc : blocks){
-						block = world.getBlockAt(tempLoc);
-						if(block.isEmpty() && block.getRelative(BlockFace.DOWN).isLiquid()){
-							block.setTypeId(111);
-							block.setMetadata("waterWalkLily", new FixedMetadataValue(this, true));
-						}
-					}
-					
-				}
-				// Player is moving East, (-1,0)
-				if (Math.round(direction.getX()) == -1 && Math.round(direction.getZ()) == 0){
-					// add the blocks in the direction player is headed
-					blocks.add(new Location(world, cloneLoc.getX() - 1.0D, cloneLoc.getY(), cloneLoc.getZ()));
-					blocks.add(new Location(world, cloneLoc.getX() - 2.0D, cloneLoc.getY(), cloneLoc.getZ()));
-					
-					// Go through list of locations and set the block to lily pad
-					for(Location tempLoc : blocks){
-						block = world.getBlockAt(tempLoc);
-						if(block.isEmpty() && block.getRelative(BlockFace.DOWN).isLiquid()){
-							block.setTypeId(111);
-							block.setMetadata("waterWalkLily", new FixedMetadataValue(this, true));
-						}
-					}
-					
-				}
-				// Player is moving South, (0, -1)
-				if (Math.round(direction.getX()) == 0 && Math.round(direction.getZ()) == -1){
-					// add the blocks in the direction player is headed
-					blocks.add(new Location(world, cloneLoc.getX(), cloneLoc.getY(), cloneLoc.getZ() - 1.0D));
-					blocks.add(new Location(world, cloneLoc.getX(), cloneLoc.getY(), cloneLoc.getZ() - 2.0D));
-					
-					// Go through list of locations and set the block to lily pad
-					for(Location tempLoc : blocks){
-						block = world.getBlockAt(tempLoc);
-						if(block.isEmpty() && block.getRelative(BlockFace.DOWN).isLiquid()){
-							block.setTypeId(111);
-							block.setMetadata("waterWalkLily", new FixedMetadataValue(this, true));
-						}
-					}
-				}
-					
-				// Player is moving West, (1, 0)
-				if (Math.round(direction.getX()) == 1 && Math.round(direction.getZ()) == 0){
-					// add the blocks in the direction player is headed
-					blocks.add(new Location(world, cloneLoc.getX() + 1.0D, cloneLoc.getY(), cloneLoc.getZ()));
-					blocks.add(new Location(world, cloneLoc.getX() + 2.0D, cloneLoc.getY(), cloneLoc.getZ()));
-					
-					// Go through list of locations and set the block to lily pad
-					for(Location tempLoc : blocks){
-						block = world.getBlockAt(tempLoc);
-						if(block.isEmpty() && block.getRelative(BlockFace.DOWN).isLiquid()){
-							block.setTypeId(111);
-							block.setMetadata("waterWalkLily", new FixedMetadataValue(this, true));
-							removeLily(blocks, player);
-							
-						}
-					}
-				}
-			}
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        final Player player = event.getPlayer();
+        Action action = event.getAction();
 
-		}
+        if (player.getLocation().getBlock().isLiquid()
+                && player.getItemInHand().equals(Material.BOOK_AND_QUILL)) {
+            player.sendMessage(ChatColor.RED
+                    + "Can't water walk when standing in water!");
+            return;
+        }
 
-	}
+        if (player.getItemInHand().getTypeId() == 387
+                && (action.toString().equals("LEFT_CLICK_AIR") || action.toString().equals("LEFT_CLICK_BLOCK"))) {
+            addPlayer(player);
 
-	@EventHandler
-	public void onPlayerInteract(PlayerInteractEvent event) {
-		final Player player = event.getPlayer();
-		Action action = event.getAction();
+            // Create new task to remove player from list in 60 seconds (roughly
+            // 1200 server ticks)
+            getServer().getScheduler().scheduleAsyncDelayedTask(this,
+                    new Runnable() {
 
-		if (player.getLocation().getBlock().isLiquid()
-				&& player.getItemInHand().equals(Material.BOOK_AND_QUILL)) {
-			player.sendMessage(ChatColor.RED
-					+ "Can't water walk when standing in water!");
-			return;
-		}
+                        public void run() {
+                            removePlayer(player);
+                        }
+                    }, 1200L);
+        }
+    }
 
-		if (player.getItemInHand().getTypeId() == 387
-				&& (action.toString().equals("LEFT_CLICK_AIR") || action
-						.toString().equals("LEFT_CLICK_BLOCK"))) {
-			addPlayer(player);
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onBlockBreak(BlockBreakEvent event) {
+        if (lilywalkers.size() > 0 && event.getBlock().getTypeId() == Material.WATER_LILY.getId()) {
+            for (Lilies lilies : lilywalkers.values()) {
+                if (lilies.contains(event.getBlock())) {
+                    event.setCancelled(true);
+                    break;
+                }
+            }
+        }
+    }
 
-			// Create new task to remove player from list in 60 seconds (roughly
-			// 1200 server ticks)
-			getServer().getScheduler().scheduleAsyncDelayedTask(this,
-					new Runnable() {
+    public void removePlayer(Player player) {
+        Lilies lilies = lilywalkers.get(player.getName());
+        if (lilies != null) {
+            lilies.remove();
+            lilywalkers.remove(player.getName());
+            player.sendMessage(ChatColor.YELLOW + "-- Water walking deactivated --");
+        }
+    }
 
-						public void run() {
-							removePlayer(player);
-						}
-					}, 1200L);
-		}
-	}
-	
-	@EventHandler
-	public void onBlockBreak(BlockBreakEvent event) {
-		
-		if (event.getBlock().hasMetadata("waterWalkLily"))
-			return;
-		
-	}
-	
-	/**
-	 * Method to remove water waking lily pads after
-	 * 3 seconds if player isn't nearby anymore
-	 */
-	public int removeLily(final List<Location> blocks, final Player player) {
-		
-		int id = getServer().getScheduler().scheduleAsyncRepeatingTask(this, new Runnable() {
+    public void addPlayer(Player player) {
+        if (!lilywalkers.containsKey(player.getName())) {
+            Lilies lilies = new Lilies();
+            lilies.move(player.getLocation().getBlock());
+            lilywalkers.put(player.getName(), lilies);
+            player.sendMessage(ChatColor.GREEN
+                    + "-- Water walking activated --");
+        } else {
+            player.sendMessage(ChatColor.RED
+                    + "-- Water walk already enabled --");
+        }
+    }
 
-			   public void run() {
-			   
-					for(Location tempLoc:blocks) {
-						// If player is more than 3 blocks away, remove it
-						if(player.getLocation().distance(tempLoc) > 3 && tempLoc.getBlock().equals(Material.getMaterial(111))){
-							tempLoc.getBlock().breakNaturally();
-						}
-					}
-				   
-			   }
-			}, 60L, 60L);
-		
-		return id;
-		
-	}
-	
-	public void removePlayer(Player player) {
-		waterWalking.remove(player.toString());
-		player.sendMessage(ChatColor.YELLOW + "-- Water walking deactivated --");
-	}
+    public class Lilies {
 
-	public void addPlayer(Player player) {
-		if (!waterWalking.contains(player.toString())) {
-			waterWalking.add(player.toString());
-			player.sendMessage(ChatColor.GREEN
-					+ "-- Water walking activated --");
-		} else {
-			player.sendMessage(ChatColor.RED
-					+ "-- Water walk already enabled --");
-		}
-	}
+        private Block center = null;
+        private HashSet<Block> blocks = new HashSet<Block>();
 
+        public void move(Block center) {
+            this.center = center;
+
+            Iterator<Block> iter = blocks.iterator();
+            while (iter.hasNext()) {
+                Block b = iter.next();
+                if (!b.equals(center)) {
+                    b.setType(Material.AIR);
+                    iter.remove();
+                }
+            }
+
+            setToLily(center);
+            setToLily(center.getRelative(BlockFace.NORTH));
+            setToLily(center.getRelative(BlockFace.SOUTH));
+            setToLily(center.getRelative(BlockFace.EAST));
+            setToLily(center.getRelative(BlockFace.WEST));
+            setToLily(center.getRelative(BlockFace.NORTH_WEST));
+            setToLily(center.getRelative(BlockFace.NORTH_EAST));
+            setToLily(center.getRelative(BlockFace.SOUTH_WEST));
+            setToLily(center.getRelative(BlockFace.SOUTH_EAST));
+        }
+
+        private void setToLily(Block block) {
+            if (block.getType() == Material.AIR) {
+                BlockState state = block.getRelative(BlockFace.DOWN).getState();
+                if ((state.getType() == Material.WATER || state.getType() == Material.STATIONARY_WATER) && state.getRawData() == (byte) 0) {
+                    block.setType(Material.WATER_LILY);
+                    blocks.add(block);
+                }
+            }
+        }
+
+        public boolean isMoved(Block center) {
+            if (this.center == null || !this.center.equals(center)) {
+                return true;
+            }
+            return false;
+        }
+
+        public boolean contains(Block block) {
+            return blocks.contains(block);
+        }
+
+        public void remove() {
+            for (Block block : blocks) {
+                block.setType(Material.AIR);
+            }
+            blocks.clear();
+        }
+    }
 }
