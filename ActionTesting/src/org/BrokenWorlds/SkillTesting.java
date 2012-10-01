8028f7a5daec0d09f54c62d55541a7a95b3746e0
Nick Krock <illusive101@hotmail.com>
2012-09-30 22:30:03 -0400

    Bardic Music WIP

--- ActionTesting/src/org/BrokenWorlds/SkillTesting.java
+++ ActionTesting/src/org/BrokenWorlds/SkillTesting.java
@@ -1,6 +1,13 @@
 package org.BrokenWorlds;
 
-import org.BrokenWorlds.Water.*;
+import org.BrokenWorlds.BardicMusic.Cheers;
+import org.BrokenWorlds.BardicMusic.Fair;
+import org.BrokenWorlds.BardicMusic.Mellow;
+import org.BrokenWorlds.BardicMusic.Stall;
+import org.BrokenWorlds.BardicMusic.Strategy;
+import org.BrokenWorlds.Water.CleansingWater;
+import org.BrokenWorlds.Water.FlashFloods;
+import org.BrokenWorlds.Water.WaterWalk;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SkillTesting extends JavaPlugin {
@@ -10,6 +17,11 @@ public class SkillTesting extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new FlashFloods(), this);
         getServer().getPluginManager().registerEvents(new WaterWalk(), this);
         getServer().getPluginManager().registerEvents(new CleansingWater(), this);
+        getServer().getPluginManager().registerEvents(new Cheers(), this);
+        getServer().getPluginManager().registerEvents(new Fair(), this);
+        getServer().getPluginManager().registerEvents(new Mellow(), this);
+        getServer().getPluginManager().registerEvents(new Stall(), this);
+        getServer().getPluginManager().registerEvents(new Strategy(), this);
     }
 
     @Override
